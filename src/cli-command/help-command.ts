f86bc551648566b1045d505a550a3d92c5e2e1de
Pavel <jab001@yandex.ru>
2022-12-21 19:42:00 +0400

    module1-task2

---
+++ src/cli-command/help-command.ts
@@ -0,0 +1,22 @@
+
+import { CliCommandInterface } from './cli-command.interface.js';
+import chalk from 'chalk';
+
+const log = console.log;
+
+export default class HelpCommand implements CliCommandInterface {
+  public readonly name = '--help';
+
+  public async execute(): Promise<void> {
+    log(`
+        ${chalk.blue(`Программа для подготовки данных для REST API сервера.`)}
+        ${chalk.yellow(`Пример:`)}
+        ${chalk.red(`main.js --<command> [--arguments]`)}
+        ${chalk.yellow(`Команды:`)}
+            ${chalk.red(`--version:`)}                   ${chalk.green(`# выводит номер версии`)}
+            ${chalk.red(`--help:`)}                      ${chalk.green(`# печатает этот текст`)}
+            ${chalk.red(`--import <path>:`)}             ${chalk.green(`# импортирует данные из TSV`)}
+            ${chalk.red(`--generate <n> <path> <url>`)}  ${chalk.green(`# генерирует произвольное количество тестовых данных`)}
+        `);
+  }
+}
---
+++ src/types/offer.type.ts
@@ -0,0 +1,21 @@
+import { Category } from './category.type.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: string;
+  image: string;
+  categories: Category[];
+  premium: Boolean;
+  rating: number;
+  typeOfRent: string;
+  rooms: number;
+  guests: number;
+  price: number;
+  features: Category[];
+  user: User;
+  comments: number;
+  coordinates: string;
+}
